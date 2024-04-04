fb0777ff8caf22cf1d4693a8ce4de5c08b0d4f0a
jingzhi218 <jingzhi0218@gmail.com>
2024-04-04 21:42:33 +0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
+import Loading from "./components/Loading/Loading";
+import { useLoading } from "./hooks/useLoading";
+import { setLoadingInterceptor} from './interceptors/loadingInterceptor';
+
+ 
+
+function App() {
+  const { showLoading, hideLoading} = useLoading();
+ 
+  useEffect(() => {
+    setLoadingInterceptor({showLoading, hideLoading});
+  }, []);
+
+  return (
+    <>
+    <Loading />
+      <Header />
+      <AppRoutes/>
+    </>
+  );
+}
+
+export default App;
